8c1ffa0291180a26567c2524c95b9841e6523806
Andrei Agustin <andreiagustin2006@gmail.com>
2026-01-02 17:59:32 +0800

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function TestNavbar(props: {title: string}) {
+  return (
+   
+
+    <nav className="navbar bg-primary">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white" href="https://www.instagram.com/and.rei_only/">@and.rei_only</a>
+      </div>
+    </nav>
+
+
+  )
+}
